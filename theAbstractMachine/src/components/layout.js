a23ed81fd04bf856f479cc2e80d997c03248900e
2092502s <lovisafsundin@gmail.com>
2021-11-06 11:57:54 +0100

    Added concepts.json

--- theAbstractMachine/src/components/layout.js
+++ theAbstractMachine/src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./navbar"
 import Footer from "./footer"
+//import background from "./background.jpg"
 import "./layout.css"
 
 export default function Layout({ children }) {
