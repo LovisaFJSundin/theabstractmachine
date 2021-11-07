c08e685899b0163579ab39de4437a33123a4c546
2092502s <lovisafsundin@gmail.com>
2021-11-07 14:24:55 +0100

    Added query to retrieve The Machine posts

--- theAbstractMachine/src/pages/the-machine/index.js
+++ theAbstractMachine/src/pages/the-machine/index.js
@@ -1,12 +1,60 @@
 import React from 'react'
 import Layout from "../../components/layout"
+import { graphql, Link } from 'gatsby'
+import styled from "styled-components";
 
-const TheMachine = () => {
+const TheMachine = ({data}) => {
+    const {allMdx:{nodes:concepts}} = data;
+    console.log(`POSTS are ${concepts}`);
     return (
         <Layout>
-            Hi welcome ot machine
+            <Wrapper>
+                {
+                    concepts.map((it,i)=>{
+                        return <ConceptEntry >{it.frontmatter.title}</ConceptEntry>
+                    })
+                }
+            </Wrapper>
         </Layout>
     )
 }
+
+const ConceptEntry = styled.h2`
+    color: #505050;
+    font-family: "USIS 1949";
+    font-size: extralarge;
+    display: block;
+`
+
+const StyledLink = styled(props => <Link {...props} />)`
+    color: #505050;
+    padding: 0px 20px 6px 20px;
+    font-family: "USIS 1949";
+    font-size: extralarge;
+`
+
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          category
+          description
+          slug
+          subtitle
+          tags
+          title
+        }
+      }
+    }
+  }
+`
 
 export default TheMachine
